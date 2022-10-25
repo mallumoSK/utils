9bc9c2c229e1479add145226848f1c8cab6586b3
mallumo <marianbanak@gmail.com>
2022-10-25 23:30:02 +0200

    improve speed

--- src/commonMain/kotlin/tk/mallumo/utils/json.kt
+++ src/commonMain/kotlin/tk/mallumo/utils/json.kt
@@ -3,6 +3,7 @@
 package tk.mallumo.utils
 
 import com.google.gson.*
+import kotlin.reflect.*
 
 private val gsonInline: Gson by lazy {
     GsonBuilder().create()
@@ -18,5 +19,7 @@ fun Any?.toJson(prettyPrint: Boolean = false): String =
     if (prettyPrint) gsonPretty.toJson(this)
     else gsonInline.toJson(this)
 
-inline fun <reified T> String.fromJson(): T = Gson().fromJson(this, T::class.java)
+inline fun <reified T : Any> String.fromJson(): T = parse(T::class)
+
+fun <T : Any> String.parse(clazz: KClass<T>) = gsonInline.fromJson(this, clazz.java)
 
