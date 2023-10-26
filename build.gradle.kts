91d3e9b13fd38dcc84f23201d3e8eea6450ea063
mallumo <develop.mallumo@gmail.com>
2023-10-26 21:53:31 +0200

    release/1.9.10-1.0.0 - upgrade to kotlin 1.9.10 compose 1.5.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,6 +82,7 @@ publishing {
     val rName = propertiesLocal["repsy.name"]
     val rKey = propertiesLocal["repsy.key"]
     repositories {
+        mavenLocal()
         maven {
             name = "repsy.io"
             url = uri("https://repo.repsy.io/mvn/${rName}/public")
