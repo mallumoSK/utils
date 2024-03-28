51da3c5e6aa8256a074c76d6bca34e082f2af5fe
mallumo <develop.mallumo@gmail.com>
2024-03-28 13:04:54 +0100

    release/1.9.23-1.0.2 - kotlin 1.9.23

--- build.gradle.kts
+++ build.gradle.kts
@@ -98,6 +98,7 @@ repositories {
     google()
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     maven("https://repo.repsy.io/mvn/mallumo/public")
     gradlePluginPortal()
 }
--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -1,11 +1,11 @@
 @Suppress("SpellCheckingInspection", "ClassName")
 object Deps {
-    const val kotlin = "1.9.10"
+    const val kotlin = "1.9.23"
     const val agp = "8.0.2"
 
     object lib {
         const val group = "tk.mallumo"
-        const val version = "1.9.10-1.0.1"
+        const val version = "1.9.23-1.0.2"
         const val artifact = "utils"
     }
 
@@ -14,7 +14,7 @@ object Deps {
     }
 
     const val gson = "com.google.code.gson:gson:2.10.1"
-    const val datetime = "org.jetbrains.kotlinx:kotlinx-datetime:0.4.1"
-    const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3"
-    const val compose = "org.jetbrains.compose.runtime:runtime:1.5.3"
+    const val datetime = "org.jetbrains.kotlinx:kotlinx-datetime:0.5.0"
+    const val coroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0"
+    const val compose = "org.jetbrains.compose.runtime:runtime:1.6.1"
 }
